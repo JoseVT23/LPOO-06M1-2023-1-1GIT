3a7d27fcb0928aa9fb13b60418e2f49de093d67a
Johan Baldeon <johan.baldeon@pucp.edu.pe>
2022-09-24 16:01:28 -0500

    Se ha añadido un comentario sobre los métodos CRUD de Product.

--- SalesController/SalesController.h
+++ SalesController/SalesController.h
@@ -13,6 +13,7 @@ namespace SalesController {
 		public:
 		/* Static se usa definir miembros que se pueden invocar
 		sin necesidad de contar con una instancia de la clase. */
+			//Métodos para el CRUD de Product
 			static int AddProduct(Product ^product); //Métodos estáticos o de clase
 			static int UpdateProduct(Product^ product);
 			static int DeleteProduct(int productId);
